7e0a5fb59bc5b22b756c67b016b72a3bb5b19461
Adley Damaceno <adleydamaceno@gmail.com>
2017-02-13 00:45:19 -0200

    Add builder and upgrader

--- default/main.js
+++ default/main.js
@@ -2,21 +2,31 @@
 var deathCreeps = require('role.deathCreeps');
 var spawnCreeps = require('role.spawnCreeps');
 var roleHarvester = require('role.harvester');
+var roleBuilder = require('role.builder');
+var roleUpgrade = require('role.upgrader');
 
 // Consts
-var ROLE_HARVESTER = "harvester";
+var CONST_HARVESTER = "harvester";
+var CONST_UPGRADER = "upgrader";
+var CONST_BUILDER = "builder";
 
 // Main Loop
 module.exports.loop = function () {
 
     deathCreeps.run();
 
-    spawnCreeps.run(ROLE_HARVESTER);
+    spawnCreeps.run(CONST_HARVESTER);
     
     for(var indx in Game.creeps) {
         var creep = Game.creeps[indx];
-        if(creep.memory.role == ROLE_HARVESTER) {
+        if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
+        }
+        if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
     
