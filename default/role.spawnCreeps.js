56e27fba59dee0df51a8d5bc3ae2188be76f498e
Adley Damaceno <adleydamaceno@gmail.com>
2017-02-13 00:37:41 -0200

    update spawns

--- default/role.spawnCreeps.js
+++ default/role.spawnCreeps.js
@@ -4,20 +4,20 @@ var smallBody = [WORK, CARRY, MOVE];
 var ROLE_HARVESTER = "harvester";
 
 var spawnCreeps = {
-     /** @param {role} creep **/
+    /** @param {role} creep **/
     run: function (role) {
 
         // Spawn a new harvester    
-        if(spawn.canCreateCreep(smallBody, null) == OK) {
-            var result = spawn.createCreep(smallBody, null, {role: ROLE_HARVESTER});
-            if(_.isString(result)) {
+        if (spawn.canCreateCreep(smallBody, null) == OK) {
+            var result = spawn.createCreep(smallBody, null, { role: ROLE_HARVESTER });
+            if (_.isString(result)) {
                 console.log('The creep name is: ' + result);
             }
             else {
                 console.log('Spawn creep error: ' + result);
             }
-        }else{
-            console.log("Can not spawn new creep: " + spawn.canCreateCreep(smallBody,null))
+        } else {
+            console.log("Can not spawn new creep: " + spawn.canCreateCreep(smallBody, null))
         }
 
     }
