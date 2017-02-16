3dcb9327c00621ba2af9a3c0f103f0e4fefb761f
Adley Damaceno <adleydamaceno@gmail.com>
2017-02-16 08:43:32 -0200

    Update code to work when just have one source

--- default/role.repair.js
+++ default/role.repair.js
@@ -30,8 +30,8 @@ var roleRepair = {
             }
         }else {
             var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
             }
         }
     }
